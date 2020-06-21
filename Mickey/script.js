3a78f1452536820058e1dbd352b052050ebe5a88
José Antonio Zahuantitla Vázquez <joseantoniozahuantitlavazquez@MacBook-Pro-de-Jose.local>
2020-06-21 00:05:20 -0500

    FEAT: Se agrego la carpeta de mickey actividad16 , se disponen las imagenes por separado en el canvas, falto la parte del formulario

---
+++ Mickey/script.js
@@ -0,0 +1,27 @@
+
+let cvs = document.getElementById("canvas");
+let ctx =cvs.getContext ("2d"); 
+
+ctx.fillStyle="red";
+
+
+ctx.rect(80,90, 150, 75); 
+ctx.fill ();
+
+/*
+ctx.arc(100, 85, 30, 0 , 2 * Math.PI ); 
+ctx.fill ();
+ctx.beginPath();
+ctx.arc(150, 135, 50, 0 , 2 * Math.PI ); 
+ctx.fill ();
+ctx.closePath(); 
+ctx.beginPath();
+ctx.arc(200, 85, 30 , 0 , 2 * Math.PI );
+ctx.fill ();
+ctx.closePath(); 
+*/
+
+/*
+ctx.arc(150, 125, 60, 0 , 2 * Math.PI ); 
+ctx.fill ();*/
+
